42ee7049efdb615dd9c9514938913bfe23ffcb88
a21oscgu <a21oscgu@student.his.se>
2022-05-11 14:43:53 +0200

    Changed id´s in the layout

--- app/src/main/java/com/example/persistence/MainActivity.java
+++ app/src/main/java/com/example/persistence/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,12 +30,20 @@ public class MainActivity extends AppCompatActivity {
         Button read = findViewById(R.id.read);
         Button delete = findViewById(R.id.delete);
 
+        final String name = findViewById(R.id.edit_text_name).toString();
+        final String color = findViewById(R.id.edit_text_color).toString();
+
+        EditText editTextTopSpeed = findViewById(R.id.edit_text_top_speed);
+        String string_from_et = editTextTopSpeed.getText().toString();
+        final int topSpeed = Integer.parseInt(string_from_et);
+
         //When the user taps (clicks) the 'Write' button the values in the EditText views
         // should be written as a single row to a SQLite database table
         write.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addCar("", "Red",300);
+                Log.d("logcat_output","WRITE");
+                addCar(name, color,topSpeed);
             }
         });
 
@@ -43,13 +52,19 @@ public class MainActivity extends AppCompatActivity {
         read.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                Log.d("logcat_output","READ");
+                printCars();
             }
         });
-
-        // Kommentarer:
-        // Och skapa t.ex. en annan knapp som tar bort berg. Name och Height kan också ha två edittexts.
-        printCars();
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("logcat_output","READ");
+                /*for(int i=1; i <= ; i++){
+                    deleteCar(i);
+                }*/
+            }
+        });
     }
     private void printCars(){
         Cursor cursor = database.query(DatabaseTables.Cars.TABLE_NAME, null, null, null, null, null, null);
@@ -73,6 +88,12 @@ public class MainActivity extends AppCompatActivity {
         values.put(DatabaseTables.Cars.COLUMN_NAME_COLOR, color);
         values.put(DatabaseTables.Cars.COLUMN_NAME_TOPSPEED, topSpeed);
         long id = database.insert(DatabaseTables.Cars.TABLE_NAME,null,values);
-        Log.d("MainActivity", "Created car with id: " + id + ", name: " + name + ", color: " + color + ", top speed: " + topSpeed + ".");
+        Log.d("logcat_output", "Created car with id: " + id + ", name: " + name + ", color: " + color + ", top speed: " + topSpeed + ".");
+    }
+
+    private int deleteCar(long id) {
+        String selection = DatabaseTables.Cars.COLUMN_NAME_ID + " = ?";
+        String[] selectionArgs = { String.valueOf(id) };
+        return database.delete(DatabaseTables.Cars.TABLE_NAME, selection, selectionArgs);
     }
 }
